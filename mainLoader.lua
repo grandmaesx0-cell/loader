8e28688a7b6f55426da471118877eee833d609fe
grandmaesx0-cell <grandmaesx0@gmail.com>
2025-08-18 06:53:37 +0300

    Update mainLoader.lua

--- mainLoader.lua
+++ mainLoader.lua
@@ -1,2 +1,3 @@
+print("Loader running.")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/grandmaesx0-cell/loader/refs/heads/main/configLoader.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/grandmaesx0-cell/loader/refs/heads/main/scriptLoader.lua"))()
