ed896abd2fca14dfdefe5ab6aa10bf0ab1b28e54
grandmaesx0-cell <grandmaesx0@gmail.com>
2025-09-10 04:09:13 +0300

    Update autoGiftConfig.lua

--- autoGiftConfig.lua
+++ autoGiftConfig.lua
@@ -1,3 +1,15 @@
 AUTO_GIFT_PETS = {"Cockatrice", "Dragonfly", "Golden Goose", "Red Fox", "Mimic Octopus"}
 AUTO_GIFT_BASE_WEIGHT = 5
 AUTO_GIFT_USERNAMES = {"babaorganik"}
+-- to add bot by bot pet
+AG_RECEIVER_MODE = "marmot" -- marmot / upgrade #DISABLED
+UPGRADE_SETTINGS = { -- DISABLED
+    ["fruit"] = "sell", -- sell / keep
+    ["petInventory"] = 60, -- 60-65-85
+    ["eggSlot"] = 3, -- 4,5,6,7,8
+    ["petSlot"] = 3 -- 4,5,6,7,8
+}
+UPGRADE_PETS = { -- DISABLED
+    "Dog", "Golden Lab", "Bunny", "Black Bunny", "Chicken", "Cat", "Deer"
+}
+UPGRADE_SEND_FRUIT_COUNT = 1 -- DISABLED
