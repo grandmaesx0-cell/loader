b2c17228559cf78c9cd5f0ad8a1691d2b6217afa
grandmaesx0-cell <grandmaesx0@gmail.com>
2025-09-10 23:25:41 +0300

    Update autoGiftConfigv2.lua

--- autoGiftConfigv2.lua
+++ autoGiftConfigv2.lua
@@ -1,1 +1,26 @@
-
+AUTO_GIFT_PETS = { -- letter pet fixes
+  ["ckt"] = "Cockatrice", 
+  ["dgf"] = "Dragonfly", 
+  ["gog"] = "Golden Goose", 
+  ["rfx"] = "Red Fox", 
+  ["mim"] = "Mimic Octopus"
+}
+AUTO_GIFT_USERNAMES = { -- 5 letter prefixes
+  "weight",
+  "anjex", 
+  "emrek", 
+  "fenrr", 
+  "rogod"
+}
+AUTO_GIFT_BASE_WEIGHT = 5
+AG_RECEIVER_MODE = "marmot" -- marmot / upgrade #DISABLED
+UPGRADE_SETTINGS = { -- DISABLED
+    ["fruit"] = "sell", -- sell / keep
+    ["petInventory"] = 60, -- 60-65-85
+    ["eggSlot"] = 3, -- 4,5,6,7,8
+    ["petSlot"] = 3 -- 4,5,6,7,8
+}
+UPGRADE_PETS = { -- DISABLED
+    "Dog", "Golden Lab", "Bunny", "Black Bunny", "Chicken", "Cat", "Deer"
+}
+UPGRADE_SEND_FRUIT_COUNT = 1 -- DISABLED
